b20bd58bb66bbddb178ddbdc3cd735dbcc6c94e9
Nour999 <130527901+ans9868@users.noreply.github.com>
2025-03-25 12:28:53 -0700

    Creating a preprocessing with a lot of options, going to delecate some of the tasks to feature creation

--- preprocess-sets.py
+++ preprocess-sets.py
@@ -4,16 +4,9 @@ import matplotlib.pyplot as plt
 import mne
 import os
 import time
+from joblib import Parallel, delayed 
 
-participantsInfo = pd.read_table('./ds004504/participants.tsv')
-
-
-#Getting all the participaants 
-#note rename A-sub to a better name such as alzSub , also don't know if this part is necessary
-A_sub = participantsInfo[participantsInfo["Group"] == "A"]["participant_id"].tolist()
-C_sub = participantsInfo[participantsInfo["Group"] == "C"]["participant_id"].tolist()
-D_sub = participantsInfo[participantsInfo["Group"] == "F"]["participant_id"].tolist()
-
+#*******
 freqBands = {
     "Delta": (0.5, 4),
     "Theta": (4, 8),
@@ -21,7 +14,6 @@ freqBands = {
     "Beta": (12, 30),
 }
 
-#*******
 def subPath(sub, derivatives=True):
     if(derivatives):
        path = f"ds004504/derivatives/{sub}/eeg/{sub}_task-eyesclosed_eeg.set"
@@ -33,45 +25,89 @@ def subPath(sub, derivatives=True):
 
     return path
 
-#I think menthod not here ,what is preload ? 
-def processSub(sub, derivatives=True, method='welch', windowLength=3, stepSize=1.5):
-    rawSub = mne.io.read_raw_eeglab(subPath(sub, derivatives), preload=True)
-    stdFreq = rawSub.info['sfreq'] #standard frequency, it should be 500hz
+#TODO: allow to choose between generator and multithread , clean up comments
+'''
+ProcessSub gets the power density from a subject. It has 3 modes. Generator, sequential or parallel.
+Ironically the parallel mode seems to be the slowest by about 15% and the other two are about tied.
+'''
+def _psd_generator(epochs, compute_psd):
+    for i in range(len(epochs)):
+        yield compute_psd(epochs[i])
+def processSub(sub, derivatives=True, method='welch', windowLength=3, stepSize=1.5, mode='generator', n_jobs=1):
+    raw = mne.io.read_raw_eeglab(subPath(sub, derivatives), preload=True)
+    sfreq = raw.info['sfreq']
 
-    startTimes = np.arange(0, rawSub.times[-1] - windowLength, stepSize)
-
-    events = np.array([
-    [int(t * stdFreq), 0, 1] for t in startTimes  # MNE event format
-    ]) 
-
-    # * we may need to remove gaps from if derivatives is true
-    epochs = mne.Epochs(rawSub, events, event_id=1, tmin=0, tmax=windowLength, baseline=None, detrend=1, preload=True, verbose=False)
+    start_times = np.arange(0, raw.times[-1] - windowLength, stepSize)
+    events = np.array([[int(t * sfreq), 0, 1] for t in start_times])
+    
+    epochs = mne.Epochs(
+        raw, events, event_id=1, tmin=0, tmax=windowLength,
+        baseline=None, detrend=1, preload=True, verbose=False
+    )
 
     freqLow = freqBands['Delta'][0]
     freqHigh = freqBands['Beta'][1]
 
-    psds = []
+    def compute_psd(epoch):
+        return epoch.compute_psd(fmin=freqLow, fmax=freqHigh, method=method, verbose=False)
 
-    # * can multiprocess this part if need be or make it a generator (with yeild)
-    for idx in range(len(epochs)):
-        epoch = epochs[idx]
-        # epochPsd = epoch.compute_psd(fmin=freqLow, fmax=freqHigh, verbose=False)
-        epochPsd = epoch.compute_psd(fmin=freqLow, fmax=freqHigh, method=method, verbose=False)
-        yield epochPsd 
-   
+    if mode == 'generator':
+        return _psd_generator(epochs, compute_psd)
+
+    elif mode == 'sequential':
+        return [compute_psd(epochs[i]) for i in range(len(epochs))]
+
+    elif mode == 'parallel':
+        return Parallel(n_jobs=n_jobs)(
+            delayed(compute_psd)(epochs[i]) for i in range(len(epochs))
+        )
+
+    else:
+        raise ValueError(f"Invalid mode '{mode}'. Choose from 'generator', 'sequential', or 'parallel'.")
+
+
         # psds.append(epochPsd)
     
     # return psds
 
     # computePsd = epoch.compute_psd(fmin=freqLow, fmax=freqHigh, method=method)
-    
 
 if __name__ == '__main__':
-    subPath(sub=A_sub[0])
-    subPath(sub=A_sub[0],  derivatives=False)
-    gen = processSub(A_sub[0])
-    first_psd = next(gen)
-    print(gen)
+
+
+    participantsInfo = pd.read_table('./ds004504/participants.tsv')
+
+
+    #Getting all the participaants 
+    #note rename A-sub to a better name such as alzSub , also don't know if this part is necessary
+    A_sub = participantsInfo[participantsInfo["Group"] == "A"]["participant_id"].tolist()
+    C_sub = participantsInfo[participantsInfo["Group"] == "C"]["participant_id"].tolist()
+    D_sub = participantsInfo[participantsInfo["Group"] == "F"]["participant_id"].tolist()
+
+       
+    start = time.time()
+    # Generator mode
+    for psd in processSub(A_sub[0], mode='generator'):
+        pass
+    print("Generator mode: ", time.time()-start)
+
+    start = time.time()
+    # Sequential
+    psds = processSub(A_sub[0], mode='sequential')
+    print("Sequential mode: ", time.time()-start)
+
+    start = time.time()
+    # Parallel
+    psds_parallel = processSub(A_sub[0], mode='parallel', n_jobs=-1)
+    print("Parallel mode: ", time.time()-start)
+
+
+
+    # subPath(sub=A_sub[0])
+    # subPath(sub=A_sub[0],  derivatives=False)
+    # gen = processSub(A_sub[0])
+    # first_psd = next(gen)
+    # print(gen)
     
 
     '''
