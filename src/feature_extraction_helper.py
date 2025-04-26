7ad3e97835f9457081913ccc6caeb233a77c46ca
Adel Nour <ans9868@nyu.edu>
2025-04-25 23:33:41 -0400

    more cleanup

--- src/feature_extraction_helper.py
+++ src/feature_extraction_helper.py
@@ -8,39 +8,21 @@ from scipy.stats import skew, kurtosis
 def bandPower(normalPsd, freqs, fmin, fmax, channel_idx=0):
    # Select the channel's PSD and the frequencies in the range
     band_mask = (freqs >= fmin) & (freqs < fmax)
-    # print(f'Normal psd shape {normalPsd.shape()}')
-    #print(f'Normal psd {normalPsd}')
     band_power = normalPsd[channel_idx, band_mask].mean()
     
     return band_power
 
 
-# check, this might just be all zeros after normliziatoin 
-def totalBandPower(normalPsd, freqs, channel_idx=0):    # But we compute the mean across all frequencies to be consistent
+def totalBandPower(normalPsd, freqs, channel_idx=0):    
     total_power = normalPsd[channel_idx, :].mean()
     
     return total_power
 
 def totalEnergy(normalPsd, freqs, channel_idx=0):
-    '''
-    Compute the total energy of the EEG signal for a specific channel.
-    Total energy is defined as the sum of the squared amplitude over time.     
-    '''
-   
     channel_signal = normalPsd[channel_idx, :]
     energy = np.sum(np.square(channel_signal))
     return energy
 
-def add_epoch_feature(rows, subjectID, epochID, feature_name, value):
-    rows.append(Row(
-        SubjectID=subjectID,
-        EpochID=epochID,
-        Electrode=None,
-        WaveBand=None,
-        FeatureName=feature_name,
-        FeatureValue=float(value),
-        table_type="epoch"
-    ))
 
 
 # * Basic Stats *
@@ -56,12 +38,6 @@ def compute_variance(x):
 def compute_rms(x):
     return np.sqrt(np.mean(x**2, axis=1))
 
-# * Higher Order Moments *
-def compute_skewness(x):
-    return np.apply_along_axis(skew, axis=1, arr=x)
-
-def compute_kurtosis(x):
-    return np.apply_along_axis(kurtosis, axis=1, arr=x)
 
 # * Hjorth Parameters *
 def compute_hjorth_mobility(x):
@@ -80,60 +56,8 @@ def compute_hjorth_index(x):
     complexity = compute_hjorth_complexity(x)
     return 2 * complexity + 100 / (2 * mobility)
 
-# * Entropy *
-def compute_app_entropy(x, m=2, r=0.2):
-    from numpy.linalg import norm
 
-    def _phi(signal, m, r):
-        N = len(signal)
-        x = np.array([signal[i:i + m] for i in range(N - m + 1)])
-        C = np.sum([np.sum(norm(x - xi, ord=np.inf, axis=1) <= r) - 1 for xi in x], axis=0)
-        return np.log(C / (N - m + 1))
 
-    return np.array([_phi(ch, m, r * np.std(ch)) - _phi(ch, m + 1, r * np.std(ch)) for ch in x])
-
-def compute_samp_entropy(x, m=2, r=0.2):
-    def _sampen(U, m, r):
-        N = len(U)
-        xmi = np.array([U[i:i + m] for i in range(N - m)])
-        xmj = np.array([U[i:i + m] for i in range(N - m)])
-        xm1i = np.array([U[i:i + m + 1] for i in range(N - m - 1)])
-        xm1j = np.array([U[i:i + m + 1] for i in range(N - m - 1)])
-        B = np.sum([np.sum(np.max(np.abs(xmi - xmj), axis=1) <= r) - 1 for i in range(len(xmi))])
-        A = np.sum([np.sum(np.max(np.abs(xm1i - xm1j), axis=1) <= r) - 1 for i in range(len(xm1i))])
-        return -np.log(A / B) if B != 0 and A != 0 else np.nan
-
-    return np.array([_sampen(ch, m, r * np.std(ch)) for ch in x])
-
-# * Fractal Dimensions *
-def compute_higuchi_fd(x, kmax=10):
-    def higuchi_fd(ts, kmax):
-        N = len(ts)
-        Lmk = []
-        for k in range(1, kmax+1):
-            Lm = []
-            for m in range(k):
-                L = 0
-                n_max = int(np.floor((N - m - 1) / k))
-                for i in range(1, n_max):
-                    L += abs(ts[m + i * k] - ts[m + (i - 1) * k])
-                L = (L * (N - 1)) / (k * n_max * k)
-                Lm.append(L)
-            Lmk.append(np.mean(Lm))
-        logL = np.log(Lmk)
-        logk = np.log(1.0 / np.arange(1, kmax + 1))
-        return np.polyfit(logk, logL, 1)[0]
-
-    return np.array([higuchi_fd(ch, kmax) for ch in x])
-
-def compute_katz_fd(x):
-    def katz_fd(ts):
-        L = np.sum(np.abs(np.diff(ts)))
-        d = np.max(np.abs(ts - ts[0]))
-        n = len(ts)
-        return np.log10(n) / (np.log10(n) + np.log10(L / d)) if d != 0 else np.nan
-
-    return np.array([katz_fd(ch) for ch in x])
 
 # * PSD-based Features *
 def spectral_entropy_from_psd(psd_band):
